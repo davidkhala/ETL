42fb34ac487681748cef9d15d48b13bacf3fb62c
David Liu <david-khala@hotmail.com>
2023-04-15 16:31:31 +0000

    add initBQ cli function

--- dbt/core/cli.sh
+++ dbt/core/cli.sh
@@ -4,4 +4,14 @@ initSilently() {
   dbt init $projectName --skip-profile-setup
   echo ./${projectName}/
 }
+initBQ() {
+
+  if [ ! -f dbt_project.yml ]; then
+    echo we assume we are in project root, but no dbt_project.yml found
+    exit 1
+  fi
+
+  curl -o profile_template.yml https://raw.githubusercontent.com/davidkhala/ETL/main/dbt/core/profile_template/gcp-data-davidkhala.yml
+  dbt init
+}
 $@
