f016e48a2f30eda4ed462689fc09a9b81d675b0b
David Liu <david-khala@hotmail.com>
2025-01-21 00:35:58 +0800

    add Set-Root

--- nifi/admin.ps1
+++ nifi/admin.ps1
@@ -1,6 +1,13 @@
 function Read-InitCredential {
     Select-String Generated logs/nifi-app*log
 }
+function Set-Root {
+    # Setup root user
+    $credential = Get-Credential # Get credentials from user input
+
+    .\bin\nifi.cmd SetSingleUserCredentials $credential.UserName $credential.Password
+
+}
 function Start {
     .\bin\nifi.cmd start
     
