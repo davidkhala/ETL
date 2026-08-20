76314c66ec5ad68b3089eb19cfb693e338d9ea00
David Liu <david-khala@hotmail.com>
2026-08-20 02:18:08 +0000

    wip digest.sh

--- datastage/remote/digest.sh
+++ datastage/remote/digest.sh
@@ -1,5 +1,8 @@
+set -eo pipefail
 ds-px-runtime() {
-	apikey=$1
+	# for private only, not for public images
+	account=$1
+	apikey=$2
 
 	ACCESS_TOKEN=$(curl -s -X POST \
 		--header "Content-Type: application/x-www-form-urlencoded" \
@@ -7,13 +10,14 @@ ds-px-runtime() {
 		--data-urlencode "grant_type=urn:ibm:params:oauth:grant-type:apikey" \
 		--data-urlencode "apikey=$apikey" \
 		"https://iam.cloud.ibm.com/identity/token" | jq -r '.access_token')
-
-	curl -s -X GET \
+	result=$(curl -s -X GET \
 		-H "accept: application/json" \
-		-H "Account: d10b01a616ed4b73a9ac8a052424a345" \
+		-H "Account: $account" \
 		-H "Authorization: Bearer $ACCESS_TOKEN" \
-		--url "https://icr.io/api/v1/images?includeIBM=false&includePrivate=true&includeManifestLists=true&vulnerabilities=true&repository=ds-px-runtime" |
-		jq '. |= sort_by(.Created) | .[length -1] | .RepoDigests[0]' | cut -d@ -f2 | tr -d '"'
+		--url "https://icr.io/api/v1/images?includeIBM=false&includePrivate=true&includeManifestLists=true&vulnerabilities=true&repository=ds-px-runtime")
+	
+	echo $result
+	echo $result | jq '. |= sort_by(.Created) | .[length -1] | .RepoDigests[0]' | cut -d@ -f2 | tr -d '"'
 }
 
 $@
